40fd6759dfb14438503e24adf3b4454254499cf2
Ajmal459001 <ajmallal362@gmail.com>
2024-11-11 10:16:06 +0530

    reduxCounter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './pages/Counter'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center mt-5'>Counter App</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
